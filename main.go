78cd85e4d282a8b1e998d1877e78bde41c48adeb
Lia Broecking <lia.broecking@stud.htwk-leipzig.de>
2020-12-02 15:40:26 +0100

    aoc first door

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	file, err := os.Open("numbers")
+
+	if err != nil {
+		log.Fatalf("failed opening file: %s", err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	var txtlines []string
+
+	for scanner.Scan() {
+		txtlines = append(txtlines, scanner.Text())
+	}
+
+	_ = file.Close()
+
+	var numbers []int
+
+	for i := 0; i < len(txtlines); i++ {
+		j, _ := strconv.Atoi(txtlines[i])
+		numbers = append(numbers, j)
+	}
+
+	//1.1
+	for _, num := range numbers {
+		for _, num2 := range numbers {
+			if num+num2 == 2020 {
+				fmt.Println(num * num2)
+			}
+		}
+	}
+
+	//1.2
+	for _, num := range numbers {
+		for _, num2 := range numbers {
+			for _, num3 := range numbers {
+				if num+num2+num3 == 2020 {
+					fmt.Println(num * num2 * num3)
+				}
+			}
+		}
+	}
+}
