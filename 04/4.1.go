9234d427ec0a7d2641233b2599e9851280bda37d
Lia Broecking <lia.broecking@stud.htwk-leipzig.de>
2020-12-04 09:57:12 +0100

    aoc day4

---
+++ 04/4.1.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
+
+func main() {
+	file_content, _ := ioutil.ReadFile("passport.txt")
+	string_content := string(file_content)
+	input := strings.Split(string_content, "\n\n")
+
+	check_elements := []string{"byr:", "iyr:", "eyr:", "hgt:", "hcl:", "ecl:", "pid:"}
+	count := 0
+	for _, key := range input {
+		check_count := 0
+		for _, element := range check_elements {
+			if strings.Contains(string(key), string(element)) {
+				check_count++
+			}
+		}
+		if check_count >= len(check_elements) {
+			count++
+		}
+	}
+
+	fmt.Println(count)
+
+}
