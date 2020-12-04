9535dfe1c345ef1c9b1c31e914de7c3082449be7
Lia Broecking <lia.broecking@stud.htwk-leipzig.de>
2020-12-04 09:00:48 +0100

    aoc day3

---
+++ 03/3.1.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("trees")
+
+	if err != nil {
+		log.Fatalf("failed opening file: %s", err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	var txtlines []string
+
+	for scanner.Scan() {
+		txtlines = append(txtlines, scanner.Text())
+	}
+
+	_ = file.Close()
+
+	currentPostition := make([]int, 2)
+	position := make([]int, 2)
+	position[0] = 3
+	position[1] = 1
+	width := len(txtlines[0])
+
+	count := 0
+
+	for currentPostition[1] < len(txtlines) {
+		y := currentPostition[1]
+		line := txtlines[y]
+
+		c := line[currentPostition[0]]
+		if c == '#' {
+			count++
+		}
+
+		currentPostition[0] = (currentPostition[0] + position[0]) % width
+		currentPostition[1] += position[1]
+	}
+
+	fmt.Println(count)
+}
---
+++ 03/3.2.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("trees")
+
+	if err != nil {
+		log.Fatalf("failed opening file: %s", err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	var txtlines []string
+
+	for scanner.Scan() {
+		txtlines = append(txtlines, scanner.Text())
+	}
+
+	_ = file.Close()
+
+	position := [][]int{
+		{1, 1},
+		{3, 1},
+		{5, 1},
+		{7, 1},
+		{1, 2},
+	}
+
+	product := 1
+
+	for i, _ := range position {
+		product *= countTrees(txtlines, position[i])
+	}
+
+	fmt.Println(product)
+}
+
+func countTrees(txtlines []string, position []int) int {
+
+	currentPostition := make([]int, 2)
+	width := len(txtlines[0])
+	count := 0
+	for currentPostition[1] < len(txtlines) {
+		y := currentPostition[1]
+		line := txtlines[y]
+		c := line[currentPostition[0]]
+		if c == '#' {
+			count++
+		}
+
+		currentPostition[0] = (currentPostition[0] + position[0]) % width
+		currentPostition[1] += position[1]
+	}
+
+	return count
+}
